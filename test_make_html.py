30d0df2ee1f16bc3697d2822117e8278e11e9e05
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-08 19:55:58 -0500

    Add config files

---
+++ test_make_html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Author:  Lyall Jonathan Di Trapani -----------------------------------
+import unittest, json
+import xml.etree.ElementTree as ET
+import make_html
+
+
+class TestFolderConfig(unittest.TestCase):
+
+    def test_get_date(self):
+        tests = (('12 Feb 2013', True),
+                 ('12 feb 2013', False),
+                 ('12 March 2013', False),
+                 ('12 Mar 13', False))
+
+
+class TestFileConfig(unittest.TestCase):
+
+    def test_parse_paragraph(self):
+        tests = ((), ())
+
+
+if __name__ == '__main__':
+    unittest.main()
