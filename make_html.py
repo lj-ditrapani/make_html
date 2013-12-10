0b8f882a1a71561dce6e3714c1d370be0a177c3c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-09 21:41:04 -0500

    First test; get markdown files

--- make_html.py
+++ make_html.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # Author:  Lyall Jonathan Di Trapani -----------------------------------
-import json
+import json, glob
 import xml.etree.ElementTree as ET
 
-TEMPLATE_DEFAULT = 'boilerplate.html'
+DEFAULTS = dict(
+    template='boilerplate.html',
+    output_directory='.',
+    css=[],
+    javascript=[],
+)
+
+
+def main():
+    for file_name in get_all_markdown_files():
+        convert(file_name)
+
+
+def get_all_markdown_files():
+    return glob.glob("*.markdown")
+
+
+def convert(file_name):
+    pass
--- test_make_html.py
+++ test_make_html.py
@@ -5,6 +5,15 @@ import xml.etree.ElementTree as ET
 import make_html
 
 
+class TestMakeHTML(unittest.TestCase):
+    
+    def test_get_all_markdown_files(self):
+        markdown_files = ['README.markdown', 'test1.markdown',
+                          'test2.markdown']
+        self.assertEqual(make_html.get_all_markdown_files(),
+                         markdown_files)
+
+
 class TestFolderConfig(unittest.TestCase):
 
     def test_get_date(self):
