047e9d48fb1762e3f9a52890e057c830c8fe9445
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-10 22:05:08 -0500

    WIP:  Write test code to exercise bible verse test

--- test_make_html.py
+++ test_make_html.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 # Author:  Lyall Jonathan Di Trapani -----------------------------------
-import unittest, json
+import unittest, json, os
 import xml.etree.ElementTree as ET
 import make_html
 
 
-class TestMakeHTML(unittest.TestCase):
+class TestBibleVerse(unittest.TestCase):
+
+    def setUp(self):
+        os.chdir('bible_verse_input')
+
+    def tearDown(self):
+        os.chdir('..')
     
+    def test_bible_verse(self):
+        make_html.main()
+        # Get expected output as etree
+        with open('../bible_verse_expected_output/test1.html') as f:
+            expected_output = f.read()
+        print expected_output[0:30]
+        # Get actual output as etree
+        # Compare 2
+        # Remove newlines??
+
     def test_get_all_markdown_files(self):
-        markdown_files = ['README.markdown', 'test1.markdown',
-                          'test2.markdown']
-        self.assertEqual(make_html.get_all_markdown_files(),
-                         markdown_files)
+        markdown_files = ['test1.markdown', 'test2.markdown']
+        self.assertEqual(make_html.get_all_markdown_files(), markdown_files)
+
 
 
 class TestFolderConfig(unittest.TestCase):
