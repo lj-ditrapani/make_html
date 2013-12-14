1a0b12a834fdc8b44b10b95310fa28f965d7c773
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-13 19:27:34 -0500

    Add attribute check and fix root

--- compare_etrees.py
+++ compare_etrees.py
@@ -2,12 +2,17 @@ class XMLException(Exception):
     pass
 
 
-def compare_etrees(element1, element2, path):
+def compare_etrees(element1, element2, path=''):
     new_path = path + '/' + element1.tag
+    if path == '':
+        path = '/'
     if element1.tag != element2.tag:
         msg = "Tags don't match at {}: {} {}"
         raise XMLException(msg.format(path, element1.tag, element2.tag))
-    print element1.attrib
+    if element1.attrib != element2.attrib:
+        format = "Attributes don't match at {}: {} {}".format
+        message = format(new_path, element1.attrib, element2.attrib)
+        raise XMLException(message)
     print element1.text
     print element1.tail
     print list(element1)
