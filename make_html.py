e5a6037036df11d81617c15657ddc6829655c5b3
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2013-12-13 22:07:15 -0500

    folder_cofig test and code passing
    Also, old compare_etrees test code is removed

--- make_html.py
+++ make_html.py
@@ -4,16 +4,45 @@ import json, glob
 import xml.etree.ElementTree as ET
 
 DEFAULTS = dict(
-    template='boilerplate.html',
+    template='template.html',
     output_directory='.',
     css=[],
     javascript=[],
 )
 
 
+def get_text(file_name):
+    with open(file_name) as f:
+        return f.read()
+
+
 def main():
+    config = get_folder_config()
     for file_name in get_all_markdown_files():
         convert(file_name)
+
+
+def get_folder_config():
+    config = DEFAULTS.copy()
+    json_config = json.load(open('config.json', 'U'))
+    config.update(json_config)
+    return config
+
+
+def get_json(file_name):
+    text = get_text('markdown/{0}.markdown'.format(file_name))
+    title = text.split('\n', 1)[0].strip()
+    config = dict(
+        title=title,
+        css=[],
+        javascript=[],
+        prettyprint=False,
+    )
+    json_file_name = 'markdown/{0}.json'.format(file_name)
+    if os.path.exists(json_file_name):
+        json_config = json.load(open(json_file_name, 'U'))
+        config.update(json_config)
+    return config
 
 
 def get_all_markdown_files():
--- test_make_html.py
+++ test_make_html.py
@@ -2,8 +2,12 @@
 # Author:  Lyall Jonathan Di Trapani -----------------------------------
 import unittest, json, os
 import xml.etree.ElementTree as ET
-from compare_etrees import compare_etrees, XMLException
 import make_html
+
+
+def get_text(file_name):
+    with open(file_name) as f:
+        return f.read()
 
 
 class TestBibleVerse(unittest.TestCase):
@@ -16,74 +20,27 @@ class TestBibleVerse(unittest.TestCase):
     
     def test_bible_verse(self):
         make_html.main()
-        # Get expected output as etree
-        expected_output = ET.parse(
+        expected_string = get_text(
             '../bible_verse_expected_output/test1.html')
-        # Get actual output as etree
-        '''
-        actual_output = ET.parse(
+        actual_string = get_text(
             '../bible_verse_actual_ouptut/test1.html')
-        '''
-        # Compare 2
-        # Remove newlines??
+        self.assertEqual(actual_string, expected_string)
+        expected_string = get_text(
+            '../bible_verse_expected_output/test2.html')
+        actual_string = get_text(
+            '../bible_verse_actual_ouptut/test2.html')
+        self.assertEqual(actual_string, expected_string)
 
     def test_get_all_markdown_files(self):
-        markdown_files = ['test1.markdown', 'test2.markdown']
-        self.assertEqual(make_html.get_all_markdown_files(), markdown_files)
+        self.assertEqual(make_html.get_all_markdown_files(),
+                         ['test1.markdown', 'test2.markdown'])
 
-
-class TestCompareEtrees(unittest.TestCase):
-
-    def run_failing_test(self, xml_str_1, xml_str_2, error_message):
-        tree1 = ET.fromstring(xml_str_1)
-        tree2 = ET.fromstring(xml_str_2)
-        with self.assertRaises(XMLException) as context:
-            compare_etrees(tree1, tree2)
-        self.assertEqual(str(context.exception), error_message)
-
-    def run_passing_test(self, xml_str_1, xml_str_2):
-        tree1 = ET.fromstring(xml_str_1)
-        tree2 = ET.fromstring(xml_str_2)
-        self.assertTrue(compare_etrees(tree1, tree2))
-    
-    def test_failing_etrees(self):
-        tests = (
-            ("<one></one>", "<two></two>",
-             "Tags don't match at /: one two"),
-            (
-                '<img src="abc" type="def"></img>',
-                '<img type="def" src="xyz" ></img>',
-                ("Attributes don't match at /img: " +
-                 "{'src': 'abc', 'type': 'def'} " +
-                 "{'src': 'xyz', 'type': 'def'}"
-                ),
-            ),
-        )
-        for str1, str2, message in tests:
-            self.run_failing_test(str1, str2, message)
-
-    def test_passing_etrees(self):
-        tests = (
-            ('<script></script>', '<script> </script>'),
-            ('<script></script>', '<script />'),
-            ('<script></script>', '<script>\n</script>'),
-            ('<p><a> </a><b> </b></p>', '<p><a> </a><b> </b></p>'),
-            (
-                '<img src="abc" type="def"></img>',
-                '<img type="def" src="abc" ></img>'
-            ),
-        )
-        for str1, str2 in tests:
-            self.run_passing_test(str1, str2)
-
-
-class TestFolderConfig(unittest.TestCase):
-
-    def test_get_date(self):
-        tests = (('12 Feb 2013', True),
-                 ('12 feb 2013', False),
-                 ('12 March 2013', False),
-                 ('12 Mar 13', False))
+    def test_folder_config(self):
+        expected_config = make_html.DEFAULTS.copy()
+        expected_config['output_directory'] = u'../bible_verse_actual_output'
+        expected_config[u'modules'] = [u'bible_verse']
+        actual_config = make_html.get_folder_config()
+        self.assertEqual(actual_config, expected_config)
 
 
 class TestFileConfig(unittest.TestCase):
